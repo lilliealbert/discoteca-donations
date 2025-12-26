2670dc689fdea0872b88aa10dc1f41f3f74973ed
Lillie Chilen <lillie.chilen@gmail.com>
2025-12-25 18:53:03 -0800

    add a donation request and some seed data

---
+++ app/controllers/donation_requests_controller.rb
@@ -0,0 +1,27 @@
+class DonationRequestsController < ApplicationController
+  before_action :set_donation_request
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @donation_request.update(donation_request_params)
+      redirect_to @donation_request, notice: "Donation request updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_donation_request
+    @donation_request = DonationRequest.find(params[:id])
+  end
+
+  def donation_request_params
+    params.require(:donation_request).permit(:request_status, :notes)
+  end
+end
