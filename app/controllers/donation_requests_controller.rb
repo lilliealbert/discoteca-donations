ecdf673f9b0c8ebaa43b55ba9fab111d6c25e928
Lillie Chilen <lillie.chilen@gmail.com>
2026-01-07 16:50:35 -0800

    stay on event show page when claiming a donation request

--- app/controllers/donation_requests_controller.rb
+++ app/controllers/donation_requests_controller.rb
@@ -8,8 +8,14 @@ class DonationRequestsController < ApplicationController
   end
 
   def update
+    was_unclaimed = @donation_request.volunteer_id.nil?
+
     if @donation_request.update(donation_request_params)
-      redirect_to @donation_request, notice: "Donation request updated successfully."
+      if was_unclaimed && @donation_request.volunteer_id.present?
+        redirect_to event_path(@donation_request.event), notice: "#{@donation_request.donor.name} claimed successfully."
+      else
+        redirect_to @donation_request, notice: "Donation request updated successfully."
+      end
     else
       render :edit, status: :unprocessable_entity
     end
