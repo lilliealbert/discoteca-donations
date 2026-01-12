0ce513859bf3138246c880b255f77c9550dbfa0f
Lillie Chilen <lillie.chilen@gmail.com>
2026-01-11 16:18:42 -0800

    add templates

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :donation_requests, only: [:show, :edit, :update]
   resources :donations, only: [:show, :edit, :update]
   resources :bulk_imports, only: [:new, :create]
+  resources :templates, only: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
