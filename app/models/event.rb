17f30e9748ffa0d154a59337a229821554998cb4
Lillie Chilen <lillie.chilen@gmail.com>
2026-01-13 11:52:03 -0800

    default to a hard-coded event for simplicity

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   has_many :donation_requests, dependent: :destroy
   has_many :donations, dependent: :destroy
+
+  CURRENT_AUCTION_NAME = "DiscoTECA 2026"
+  scope :default_event, -> { where(name: CURRENT_AUCTION_NAME) }
 end
